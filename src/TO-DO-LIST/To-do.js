f674c6e56e3870973c6ff92dbca0fc1fdfa9d3ee
shivank goyal <goyal.shivank790@gmail.com>
2021-04-21 22:41:58 +0530

    append button created

--- src/TO-DO-LIST/To-do.js
+++ src/TO-DO-LIST/To-do.js
@@ -1,14 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import "./to-do.css"
 
 const Todo = () =>{
+
+
+    let[value,setvalue] = useState("");
+     const function2 = (event) => {
+        
+        setvalue(event.target.value);
+    }
+
+    const function1 = () =>{
+
+        let input = document.getElementById("input");
+        const list = document.createElement("li");
+        list.innerHTML = value;
+        const node = document.getElementById("list1");
+        node.appendChild(list);
+        input.value="";
+        
+    }
 
 return(
 <>
 <div className="todo-container">
 <h1 className="heading1"> TO-DO-LIST</h1>
-<input type="text" className="inpt" placeholder="Create a task" ></input><button className="btn">➕</button>
-
+<input id="input" type="text" className="inpt" placeholder="Create a task" onChange={function2}></input>
+<button className="btn" onClick={function1}>➕</button>
+<ul id="list1">
+    
+</ul>
 </div>
 
 
