0cd6397e3ffdefe317a05d66eaf1eb6c18c22815
shivank goyal <goyal.shivank790@gmail.com>
2021-04-21 19:41:47 +0530

    UI added

---
+++ src/TO-DO-LIST/To-do.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./to-do.css"
+
+const Todo = () =>{
+
+return(
+<>
+<div className="todo-container">
+<h1 className="heading1"> TO-DO-LIST</h1>
+<input type="text" className="inpt" placeholder="Create a task" ></input><button className="btn">➕</button>
+
+</div>
+
+
+</>
+
+
+);
+
+}  
+
+
+export default Todo
