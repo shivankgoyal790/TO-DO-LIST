a5a014d6f1d764e6f05a663a2f8d0cc04d85849b
shivank goyal <goyal.shivank790@gmail.com>
2021-04-22 22:20:30 +0530

    mapping and responsiveness added

--- src/TO-DO-LIST/To-do.js
+++ src/TO-DO-LIST/To-do.js
@@ -4,39 +4,40 @@ import "./to-do.css"
 const Todo = () =>{
 
 
-    let[value,setvalue] = useState("");
-     const function2 = (event) => {
-        
-        setvalue(event.target.value);
+    const[value,setvalue] = useState("");
+    const[items,setitems] = useState([]);    
+   
+   
+    const function2 = (event) => {
+            setvalue(event.target.value);         
     }
-
     const function1 = () =>{
 
-        let input = document.getElementById("input");
-        const list = document.createElement("li");
-        list.innerHTML = value;
-        const node = document.getElementById("list1");
-        node.appendChild(list);
-        input.value="";
-        
+        setitems((prevValue) =>{
+
+            return [...prevValue,value];
+        } ) 
+        setvalue("");    
     }
 
-return(
-<>
-<div className="todo-container">
-<h1 className="heading1"> TO-DO-LIST</h1>
-<input id="input" type="text" className="inpt" placeholder="Create a task" onChange={function2}></input>
-<button className="btn" onClick={function1}>➕</button>
-<ul id="list1">
-    
-</ul>
-</div>
+    const function3 = () =>{
 
 
-</>
-
-
-);
+    }
+    return(
+    <>
+        <div className="todo-container">
+        <h1 className="heading1"> TO-DO-LIST</h1>
+        <input id="input" type="text" className="inpt" placeholder="Create a task" onChange={function2} value={value}></input>
+        <button className="btn" onClick={function1}>➕</button>
+        <ul id="list1">
+            {items.map( (itemvalue , index) => {
+                return <li key={index}>{itemvalue} <button onclick={function3} className="btn2">⚔️</button>  </li>
+            })}
+        </ul>
+        </div>
+    </>
+    );
 
 }  
 
